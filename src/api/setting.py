6297f51dc35eabdd78c9122559a2d6645411e28d
蘇品甄 Annie Su <pznsstudio@gmail.com>
2025-04-22 17:18:58 +0800

    Update setting.py

--- src/api/setting.py
+++ src/api/setting.py
@@ -11,7 +11,7 @@ DESCRIPTION = """
 Use OpenAI-Compatible RESTful APIs for Amazon Bedrock models.
 """
 
-DEBUG = os.environ.get("DEBUG", "false").lower() != "true"
+DEBUG = os.environ.get("DEBUG", "false").lower() != "false"
 AWS_REGION = os.environ.get("AWS_REGION", "ap-northeast-1")
 DEFAULT_MODEL = os.environ.get(
     "DEFAULT_MODEL", "anthropic.claude-3-sonnet-20240229-v1:0"
